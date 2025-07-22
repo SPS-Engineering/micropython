6ac38b6e26443d28ca9959af8b2da26265fbb127
Miss D <40424787+knrdsps@users.noreply.github.com>
2025-07-22 16:08:38 +0100

    Update switch.py

--- switch.py
+++ switch.py
@@ -1,15 +1,11 @@
 from machine import Pin
 import utime
-                                                      
-#  led_g = Pin(22, Pin.OUT)
 
-#  led_g.value(0)
-button = Pin(19, Pin.In, PULL_UP)
+button = Pin(14, Pin.IN, Pin.PULL_UP)
 
 while True:
-    button.when_pressed == 0:
+    if button.value() == 0:
         print("Button is Pressed")
     else:
         print("Button is not Pressed")
-utime.sleep(0.1) 
-
+    utime.sleep(0.1)
